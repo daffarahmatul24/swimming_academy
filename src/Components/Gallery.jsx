fa8e5da404da2c25af6f7b33b230600d47b5453d
Daffa Rahmatul <davarahmatul05@gmail.com>
2026-05-19 14:48:20 +0700

    fix gallery img paths

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -4,10 +4,10 @@ import Masonry from 'react-responsive-masonry';
 
 export default function Gallery() {
   const images = [
-    './public/images/galery1.jpeg',
-    './public/images/galery2.jpeg',
-    './public/images/galery3.jpeg',
-    './public/images/galery4.jpeg',
+    '/public/images/galery1.jpeg',
+    '/public/images/galery2.jpeg',
+    '/public/images/galery3.jpeg',
+    '/public/images/galery4.jpeg',
   ];
 
   return (
